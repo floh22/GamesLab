c9c1ca60b51094fc04dd11f60f6b5cbfe317f85b
ga87xih <Paul@Studysmarter.de>
2021-12-11 16:55:57 +0100

    Second UI Update and Minion Target Switch Menu

--- Assets/Scipts/GameData.cs
+++ Assets/Scipts/GameData.cs
@@ -10,9 +10,25 @@ public class GameData : MonoBehaviour
         GAME_END = 2,
     }
 
-    public bool GameIsPaused;
-    public Levels CurrentLevel;
+    public enum Player {
+        RED = 0,
+        GREEN = 1,
+        YELLOW = 2,
+        BLUE = 3,
+    }
 
+    public string[] PlayerNames;
+
+    public static float[][] PlayerColors = new float[4][];
+    public bool GameIsPaused;
+    public bool IsMinionSwitchMenuOpen;
+    public Levels CurrentLevel;
+    public Player CurrentPlayer;
+    public Player SelectedMinionTarget;
+
+    // R  G
+    //
+    // B  Y
     void Awake()
     {
         if(Instance == null) {
@@ -20,10 +36,33 @@ public class GameData : MonoBehaviour
 
             this.CurrentLevel  = (Levels)1;
             this.GameIsPaused = false;
+            this.IsMinionSwitchMenuOpen = true;
+
+            PlayerColors[0] = new float[] {0.7169812F, 0.131221F, 0.131221F, 1};
+            PlayerColors[1] = new float[] {0.05490196F, 0.4823529F, 0.06666667F, 1};
+            PlayerColors[2] = new float[] {0.972549F, 0.9215686F, 0.05882353F, 1};
+            PlayerColors[3] = new float[] {0.1294118F, 0.1921569F, 0.7176471F, 1};
+
+            PlayerNames = new string[4];
+
+            SetCurrentPlayer(3);
+            SetPlayers("Redboy", "Greendude", "Yellowman", "Blueguy");
+            
 
             DontDestroyOnLoad(gameObject);
         } else {
             Destroy(gameObject);
         }
+    }
+
+    void SetPlayers(string NameRed, string NameGreen, string NameYellow, string NameBlue) {
+        PlayerNames[0] = NameRed;
+        PlayerNames[1] = NameGreen;
+        PlayerNames[2] = NameYellow;
+        PlayerNames[3] = NameBlue;
+    }
+
+    void SetCurrentPlayer(int player) {
+        CurrentPlayer = (Player)player;
     }
 }
--- Assets/Scipts/GameManager.cs
+++ Assets/Scipts/GameManager.cs
@@ -2,11 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject PauseMenuUI;
+    public GameObject MinionSwitchMenuUI;
     public GameObject IngameUI;
+    public Image MinionSwitchButtonImage;
+
+    private TMP_Text TargetPlayerMinionSwitchText;
+    private Image TargetPlayerMinionSwitchImage;
+
+    private TMP_Text[] PlayerMinionSwitchTexts;
+    private Image[] PlayerMinionSwitchImages;
+
+    void Start() {
+        MinionSwitchMenuUI.SetActive(true);
+        TargetPlayerMinionSwitchText = GameObject.Find("Player1_Text").GetComponent<TMP_Text>();
+        TargetPlayerMinionSwitchImage = GameObject.Find("Player1_SwitchImage").GetComponent<Image>();
+
+        PlayerMinionSwitchTexts = new TMP_Text[2];
+        PlayerMinionSwitchImages = new Image[2];
+        PlayerMinionSwitchTexts[0] = GameObject.Find("Player2_Text").GetComponent<TMP_Text>();
+        PlayerMinionSwitchImages[0] = GameObject.Find("Player2_SwitchImage").GetComponent<Image>();
+        PlayerMinionSwitchTexts[1] = GameObject.Find("Player3_Text").GetComponent<TMP_Text>();
+        PlayerMinionSwitchImages[1] = GameObject.Find("Player3_SwitchImage").GetComponent<Image>();
+        MinionSwitchMenuUI.SetActive(false);
+
+        SetMinionTarget(GetNextPlayerClockwise(GameData.Instance.CurrentPlayer), true);
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,6 +54,76 @@ public class GameManager : MonoBehaviour
             PauseMenuUI.SetActive(true);
             IngameUI.SetActive(false);
             GameData.Instance.GameIsPaused = true;
+        }
+    }
+
+    public void ToggleMinionSwitchMenu() {
+        if(GameData.Instance.IsMinionSwitchMenuOpen) {
+            MinionSwitchMenuUI.SetActive(false);
+            IngameUI.SetActive(true);
+            GameData.Instance.IsMinionSwitchMenuOpen = false;
+        } else {
+            MinionSwitchMenuUI.SetActive(true);
+            IngameUI.SetActive(false);
+            GameData.Instance.IsMinionSwitchMenuOpen = true;
+        }
+    }
+
+    public void SetMinionTarget(GameData.Player target, bool toggleMenu = true) {
+        GameData.Instance.SelectedMinionTarget = target;
+        MinionSwitchButtonImage.color = GetColor(target);
+
+        UpdateLabels();
+
+        if(toggleMenu) {
+            ToggleMinionSwitchMenu();
+        }
+    }
+
+    public void UpdateMinionTarget(int target) {
+        int counter = 0;
+        for(int i = 0; i < 4; i++) {
+            if((GameData.Player)i != GameData.Instance.CurrentPlayer && (GameData.Player)i != GameData.Instance.SelectedMinionTarget) {
+                if(target == counter) {
+                    SetMinionTarget((GameData.Player)i);
+                    return;
+                } else {
+                    counter++;
+                }
+            }
+
+        }
+    }
+
+    public void UpdateLabels() {
+        TargetPlayerMinionSwitchText.text = WrapText(GameData.Instance.PlayerNames[(int)GameData.Instance.SelectedMinionTarget]);
+        TargetPlayerMinionSwitchImage.color = GetColor(GameData.Instance.SelectedMinionTarget);
+
+        int counter = 0;
+        for(int i = 0; i < 4; i++) {
+            if((GameData.Player)i != GameData.Instance.CurrentPlayer && (GameData.Player)i != GameData.Instance.SelectedMinionTarget) {
+                PlayerMinionSwitchTexts[counter].text = WrapText(GameData.Instance.PlayerNames[i]);
+                PlayerMinionSwitchImages[counter].color = GetColor((GameData.Player)i);
+                counter++;
+            }
+
+        }
+    }
+
+    GameData.Player GetNextPlayerClockwise(GameData.Player player) {
+        return (GameData.Player) ((int)player - 1 % 4);
+    }
+
+    Color GetColor(GameData.Player player) {
+        float[] color = GameData.PlayerColors[(int)player];
+        return new Color(color[0], color[1], color[2], color[3]);
+    }
+
+    string WrapText(string text) {
+        if(text.Length > 7) {
+            return text.Substring(0, 7) + "...";
+        } else {
+            return text;
         }
     }
 }
