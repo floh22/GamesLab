a63fa8237e318e1f47c6278b49908e0fb259a604
Paul Pillau <Paul.Online@Pillau.com>
2022-01-17 23:09:31 +0100

    Scoreboard Update - Half Working & Pages working & SetMinionTarget UI half working

--- Assets/Scripts/Network/GameStateController.cs
+++ Assets/Scripts/Network/GameStateController.cs
@@ -20,36 +20,33 @@ namespace Network
     public class GameStateController : MonoBehaviourPunCallbacks, IOnEventCallback
     {
         public static GameStateController Instance;
-        
+
         [CanBeNull] private MasterController controller;
-        
+
         public const byte ChangeMinionTargetEventCode = 1;
         public const byte DamageGameUnitEventCode = 3;
         public const byte ChannelSlenderManEventCode = 4;
-
 
         public static UnityEvent LocalPlayerSpawnEvent = new();
 
         public static void SendChangeTargetEvent(GameData.Team team, GameData.Team target)
         {
-            object[] content = { team, target }; 
-            RaiseEventOptions raiseEventOptions = new() { Receivers = ReceiverGroup.Others }; 
+            object[] content = {team, target};
+            RaiseEventOptions raiseEventOptions = new() {Receivers = ReceiverGroup.Others};
             PhotonNetwork.RaiseEvent(ChangeMinionTargetEventCode, content, raiseEventOptions, SendOptions.SendReliable);
         }
-        
-        
-        [Header("Player Data")]
-        [SerializeField]
+
+
+        [Header("Player Data")] [SerializeField]
         private GameObject playerPrefab;
 
         [SerializeField] private GameObject abilityPrefab;
 
         [SerializeField] private GameObject spawnPointHolder;
-        
-        
-        [Header("Minion Data")]
-        
-        [SerializeField] private MinionValues minionValues;
+
+        [Header("Minion Data")] [SerializeField]
+        private MinionValues minionValues;
+
         [SerializeField] private GameObject minionPrefab;
         [SerializeField] private GameObject minionSpawnPointHolder;
         [SerializeField] private GameObject minionPaths;
@@ -59,14 +56,13 @@ namespace Network
         public Dictionary<GameData.Team, BaseBehavior> Bases;
         public Dictionary<GameData.Team, HashSet<Minion>> Minions;
         public HashSet<IGameUnit> GameUnits;
-        
+
         public Dictionary<GameData.Team, GameData.Team> Targets;
 
 
         private bool hasLeft = false;
-        
-        #region Photon Callbacks
 
+        #region Photon Callbacks
 
         /// <summary>
         /// Called when the local player left the room. We need to load the launcher scene.
@@ -86,7 +82,6 @@ namespace Network
 
         #region Public Methods
 
-
         public void LeaveRoom()
         {
             if (hasLeft)
@@ -100,7 +95,7 @@ namespace Network
                 PhotonView photonView = PhotonView.Get(this);
                 photonView.RPC("NewMasterClient", RpcTarget.Others, nextMasterClient.ActorNumber);
             }
-            
+
             PhotonNetwork.LeaveRoom();
         }
 
@@ -113,9 +108,8 @@ namespace Network
             }
         }
 
-
         #endregion
-        
+
         void Start()
         {
             Instance = this;
@@ -134,52 +128,53 @@ namespace Network
             Targets = new Dictionary<GameData.Team, GameData.Team>();
             GameUnits = new HashSet<IGameUnit>();
 
-            if (playerPrefab == null) { // #Tip Never assume public properties of Components are filled up properly, always check and inform the developer of it.
-
-                Debug.LogError("<Color=Red><b>Missing</b></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
-            } else {
-
-
-                if (PlayerController.LocalPlayerInstance==null)
+            if (playerPrefab == null)
+            {
+                // #Tip Never assume public properties of Components are filled up properly, always check and inform the developer of it.
+                Debug.LogError(
+                    "<Color=Red><b>Missing</b></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",
+                    this);
+            }
+            else
+            {
+                if (PlayerController.LocalPlayerInstance == null)
                 {
                     Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                     Vector3 pos = spawnPointHolder.transform.Find(PersistentData.Team.ToString()).transform.position;
 
                     // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
                     GameObject pl = PhotonNetwork.Instantiate(this.playerPrefab.name, pos, Quaternion.identity, 0);
-                    
+
                     pl.transform.Find("FogOfWarVisibleRangeMesh").gameObject.SetActive(true);
 
-                    GameObject abilityGo = PhotonNetwork.Instantiate("PlayerAbilities", Vector3.zero, Quaternion.identity);
+                    GameObject abilityGo =
+                        PhotonNetwork.Instantiate("PlayerAbilities", Vector3.zero, Quaternion.identity);
                     abilityGo.GetComponent<GameUnitFollower>().StartFollowing(pl.transform);
                 }
-                else{
-
+                else
+                {
                     Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
                 }
-
-
             }
-            
+
             GameObject baseHolder = GameObject.Find("Bases");
 
-            foreach (GameData.Team team in (GameData.Team[])Enum.GetValues(typeof(GameData.Team)))
+            foreach (GameData.Team team in (GameData.Team[]) Enum.GetValues(typeof(GameData.Team)))
             {
                 Minions.Add(team, new HashSet<Minion>());
-                
+
                 //Set default target to opposing team
-                Targets.Add(team, (GameData.Team)(((int)team + 2) % 4));
+                Targets.Add(team, (GameData.Team) (((int) team + 2) % 4));
 
                 if (baseHolder == null || baseHolder.Equals(null)) continue;
                 BaseBehavior bb = baseHolder.transform.Find(team.ToString()).GetComponent<BaseBehavior>();
                 Bases.Add(team, bb);
                 GameUnits.Add(bb);
-
             }
 
 
             StartCoroutine(InitPlayersThisRound());
-            
+
             if (!PhotonNetwork.IsMasterClient) return;
             try
             {
@@ -192,9 +187,8 @@ namespace Network
             {
                 Debug.LogError("Could not create master controller. Server functionality will not work");
             }
-
         }
-        
+
         public override void OnEnable()
         {
             base.OnEnable();
@@ -215,7 +209,7 @@ namespace Network
                 QuitApplication();
             }
         }
-        
+
         private IEnumerator InitPlayersThisRound()
         {
             //Wait 2 seconds to init players to let everyone join
@@ -226,7 +220,7 @@ namespace Network
                 PlayerController pc = playerGo.GetComponent<PlayerController>();
                 return new KeyValuePair<GameData.Team, PlayerController>(pc.Team, pc);
             }).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            
+
             //Add them to gameUnits
             Debug.Log($"{Players.Count} Players found");
             int preSize = GameUnits.Count;
@@ -245,7 +239,7 @@ namespace Network
                 Debug.LogError("Something went wrong adding players to GameUnit list");
             }
         }
-        
+
         public void QuitApplication()
         {
             Application.Quit();
@@ -256,29 +250,29 @@ namespace Network
             return spawnPointHolder.transform.Find(team.ToString()).transform.position;
         }
 
-        
+
         public void OnEvent(EventData photonEvent)
         {
             byte eventCode = photonEvent.Code;
 
             if (eventCode == ChangeMinionTargetEventCode)
             {
-                object[] data = (object[])photonEvent.CustomData;
+                object[] data = (object[]) photonEvent.CustomData;
 
-                GameData.Team team = (GameData.Team)data[0];
-                GameData.Team target = (GameData.Team)data[1];
+                GameData.Team team = (GameData.Team) data[0];
+                GameData.Team target = (GameData.Team) data[1];
 
                 SetMinionTarget(team, target);
             }
-            
+
             if (eventCode == DamageGameUnitEventCode)
             {
                 Debug.Log("Damage Event");
-                object[] data = (object[])photonEvent.CustomData;
+                object[] data = (object[]) photonEvent.CustomData;
 
-                int sourceID = (int)data[0];
-                int targetID = (int)data[1];
-                float damage = (float)data[2];
+                int sourceID = (int) data[0];
+                int targetID = (int) data[1];
+                float damage = (float) data[2];
 
                 IGameUnit? source = null;
                 IGameUnit? target = null;
@@ -295,14 +289,17 @@ namespace Network
 
                 if (target == null || source == null)
                 {
-                    Debug.Log($"target or source null: source: {source?.NetworkID}, target: {target?.NetworkID}. sourceID: {sourceID}, targetID: {targetID}");
-                    Debug.Log(GameUnits.Select(unit => unit.NetworkID).ToList().Aggregate("GameUnits: ", (current, item) => current + (item + ", ")));
-                    
+                    Debug.Log(
+                        $"target or source null: source: {source?.NetworkID}, target: {target?.NetworkID}. sourceID: {sourceID}, targetID: {targetID}");
+                    Debug.Log(GameUnits.Select(unit => unit.NetworkID).ToList()
+                        .Aggregate("GameUnits: ", (current, item) => current + (item + ", ")));
+
                     return;
                 }
+
                 Debug.Log("showing damage dealt");
                 target.DoDamageVisual(source, damage);
-                
+
                 //I am the owner. Deal the damage. This will get synced by photon
                 if (target.OwnerID == PhotonNetwork.LocalPlayer.ActorNumber)
                 {
@@ -311,22 +308,19 @@ namespace Network
                 }
             }
         }
-        
-        
-        
+
+
         void SetMinionTarget(GameData.Team team, GameData.Team target)
         {
-
             Targets[team] = target;
-            
+
             if (!PhotonNetwork.IsMasterClient) return;
-            
+
             //For now, have all minions instantly switch agro. Maybe change this over so only future minions switch agro?
             foreach (Minion minionBehavior in Minions[team].NotNull())
             {
                 minionBehavior.SetTargetTeam(target);
             }
         }
-        
     }
 }
