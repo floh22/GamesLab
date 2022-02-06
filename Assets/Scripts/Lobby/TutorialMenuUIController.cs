7cb8946ecd77b8bfde4bc158e1109814e06e1845
Kyryll Vasylenko <vasilenkowm@gmail.com>
2022-02-06 17:53:45 +0100

    Add tutorial scene

--- Assets/Scripts/Lobby/TutorialMenuUIController.cs
+++ Assets/Scripts/Lobby/TutorialMenuUIController.cs
@@ -14,10 +14,9 @@ namespace Lobby
     {
         public static TutorialMenuUIController Instance;
         
-        [SerializeField] private Button howToPlay;
-        [SerializeField] private Button slenderman;
-        [SerializeField] private Button bases;
-        [SerializeField] private Button exitTutorial;
+        [SerializeField] private GameObject howToPlayView;
+        [SerializeField] private GameObject slendermanView;
+        [SerializeField] private GameObject basesView;
 
         void Start()
         {
@@ -32,14 +31,23 @@ namespace Lobby
 
         public void HowToPlay()
         {
+            howToPlayView.SetActive(true);
+            slendermanView.SetActive(false);
+            basesView.SetActive(false);
         }
 
         public void Slenderman()
         {
+            howToPlayView.SetActive(false);
+            slendermanView.SetActive(true);
+            basesView.SetActive(false);
         }
 
         public void Bases()
         {
+            howToPlayView.SetActive(false);
+            slendermanView.SetActive(false);
+            basesView.SetActive(true);
         }
 
         public void BackToLobby()
